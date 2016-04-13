da7b6edc192e86511eea7568b2f7ef5f4bde06f2
brettratner <ratnerb1@tcnj.edu>
2016-04-13 17:19:05 -0400

    update

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 
 // Set the database access information
 
-		$db_user = '***REMOVED***';
-		$db_password = '***REMOVED***';
+		$db_user = 'ENTER DATA-BASE USER HERE';
+		$db_password = 'ENTER PASSWORD HERE';
 		$db_host = 'localhost';
-		$db_name = '***REMOVED***';
+		$db_name = 'ENTER DATA-BASE HERE';
 ?>

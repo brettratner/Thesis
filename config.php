e56d6b6a95d94edf78c5ff35d5c3b5a4ec1b1c84
brettratner <brettratner@users.noreply.github.com>
2016-04-13 17:20:27 -0400

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Set the database access information
-
-		$db_user = 'ENTER DATA-BASE USER HERE';
-		$db_password = 'ENTER PASSWORD HERE';
-		$db_host = 'localhost';
-		$db_name = 'ENTER DATA-BASE HERE';
-?>
